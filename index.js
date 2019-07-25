d9a0b331d94764fb09fa740a729c1fbf5749f3ce
Christina Park <christinapark0117@gmail.com>
2019-07-25 18:31:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holidays, name) {
-  return "Happy ${holidays}, ${name}!"
+  return `Happy ${holidays}, ${name}!`
 }
 function holidayCountdown(days, holiday) {
-  return "It's ${days} days until ${holiday}!"
+  return `It's ${days} days until ${holiday}!`
 }
